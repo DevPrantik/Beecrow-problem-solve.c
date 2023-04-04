1e45a1b7eeb279485c9f82f93daa2a6752c72052
DevPrantik <126088148+DevPrantik@users.noreply.github.com>
2023-04-05 01:19:03 +0600

    Add files via upload

---
+++ 1011.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int R;
+    double pi,result;
+    pi = 3.14159;
+    scanf("%d",&R);
+   result = (4/3.0)*pi*R*R*R;
+    printf("VOLUME = %.3lf\n",result);
+    return 0;
+}
+
+
+
+
