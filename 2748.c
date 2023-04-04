4170bb1b47dc777b680c85f7d4b1f3c8d449333b
DevPrantik <126088148+DevPrantik@users.noreply.github.com>
2023-04-05 00:39:03 +0600

    Add files via upload

---
+++ 2748.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+int main() {
+    printf("---------------------------------------\n");
+    printf("|        ROBERT                     |\n");
+    printf("|                                     |\n");
+    printf("|        5786                         |\n");
+    printf("|                                     |\n");
+    printf("|        UNIFEI                       |\n");
+    printf("---------------------------------------\n");
+}
