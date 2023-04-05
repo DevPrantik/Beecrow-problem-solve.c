33022b90f64ada848444ab0ca7fc551e45d40c2f
DevPrantik <126088148+DevPrantik@users.noreply.github.com>
2023-04-05 23:57:00 +0600

    Add files via upload

---
+++ 1008 - Salary.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    double c;
+    scanf("%d %d %lf",&a, &b, &c);
+    double x= b*c ;
+    printf("NUMBER = %d\n",a);
+
+    printf("SALARY = U$ %.2lf\n",x);
+return 0;
+}
+
